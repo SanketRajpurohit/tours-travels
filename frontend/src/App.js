90d89e8ff3a8b7437c74ae09f9e33ae425596891
SanketRajpurohit <sankrp5@gmail.com>
2025-08-17 22:20:23 +0530

    Initial commit: frontend + backend + gitignore + requirements + readme

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-blue-600 text-blue-100">
+        <h1 className="text-4xl font-bold">Hello Tailwind v4!</h1>
+      </div>
+  );
+}
+
+export default App;
